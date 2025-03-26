4c02532465ed838aac5e8f684eebc95a0238ee2f
Miguel <miguel.perassoli@gmail.com>
2025-03-26 00:10:40 -0300

    pronto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int j, m, c, i, t;
+Console.WriteLine("Escreva seu capital");
+c = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine("Escreva seu taxa de juros");
+i = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine("Escreva seu tempo");
+t = Convert.ToInt32(Console.ReadLine()!);
+
+j = c * i * t;
+
+m = c + j;
+
+Console.WriteLine(@$"Seu montante é {m} e seu juros é {j}");
